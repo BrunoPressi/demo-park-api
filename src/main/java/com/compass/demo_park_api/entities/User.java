9bcc0465a56d93c38a692e01e8b7b622a8981312
Bruno Pressi <brunopressi2012@gmail.com>
2025-04-22 16:38:59 -0300

    feat: add User Entity

---
+++ src/main/java/com/compass/demo_park_api/entities/User.java
@@ -0,0 +1,61 @@
+package com.compass.demo_park_api.entities;
+
+import com.compass.demo_park_api.entities.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false, unique = false, length = 200)
+    private String password;
+
+    @Column(name = "role", nullable = false, length = 25)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "date_creation")
+    private LocalDateTime dateCreation;
+
+    @Column(name = "date_modify")
+    private LocalDateTime dateModify;
+
+    @Column(name = "create_by")
+    private String createBy;
+
+    @Column(name = "modify_by")
+    private String modifyBy;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                '}';
+    }
+}
---
+++ src/main/java/com/compass/demo_park_api/entities/enums/Role.java
@@ -0,0 +1,8 @@
+package com.compass.demo_park_api.entities.enums;
+
+public enum Role {
+
+    ROLE_ADMIN,
+    ROLE_CLIENT
+
+}
