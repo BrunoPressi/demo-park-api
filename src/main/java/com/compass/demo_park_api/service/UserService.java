dd669ca24bd8787fe4ca247533dff1e493a60110
Bruno Pressi <brunopressi2012@gmail.com>
2025-04-23 12:53:03 -0300

    feat: add create user resource

--- src/main/java/com/compass/demo_park_api/service/UserService.java
+++ src/main/java/com/compass/demo_park_api/service/UserService.java
@@ -1,6 +1,8 @@
 package com.compass.demo_park_api.service;
 
+import com.compass.demo_park_api.entity.User;
 import com.compass.demo_park_api.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,5 +13,9 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-}
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 
+}
--- src/main/java/com/compass/demo_park_api/web/controller/UserController.java
+++ src/main/java/com/compass/demo_park_api/web/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.compass.demo_park_api.web.controller;
 
+import com.compass.demo_park_api.entity.User;
 import com.compass.demo_park_api.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User obj) {
+        User user = userService.save(obj);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 
 }
