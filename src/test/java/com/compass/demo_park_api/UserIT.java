9c9e3d17d9356282fc1427c7aca9e588c89c86fd
Bruno Pressi <brunopressi2012@gmail.com>
2025-04-25 13:11:12 -0300

    test(user): end-to-end test to create a new user

--- src/test/java/com/compass/demo_park_api/UserIT.java
+++ src/test/java/com/compass/demo_park_api/UserIT.java
@@ -1,7 +1,12 @@
 package com.compass.demo_park_api;
 
+import com.compass.demo_park_api.entity.User;
+import com.compass.demo_park_api.web.dto.UserCreateDto;
+import com.compass.demo_park_api.web.dto.UserResponseDto;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -13,6 +18,23 @@ public class UserIT {
     @Autowired
     WebTestClient testClient;
 
+    @Test
+    public void createUser_nameAndPasswordAreValid_returnUserCreatedWithStatus201() {
+        UserResponseDto responseBody = testClient
+                .post()
+                .uri("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDto("tody@gmail.com", "123456"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(UserResponseDto.class)
+                .returnResult().getResponseBody();
 
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("tody@gmail.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENT");
+
+    }
 
 }
